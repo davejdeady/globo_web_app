b97602b69ea428a5e6043d6ecb64d98b8bff1a95
davejdeady <davidjohndeady@outlook.com>
2025-08-28 16:32:40 +0100

    for modules

--- network.tf
+++ network.tf
@@ -15,6 +15,24 @@ data "aws_availability_zones" "available" {
 ##################################################################################
 
 # NETWORKING #
+module "app" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  cidr = var.vpc_cidr_block
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, var.vpc_public_subnet_count)
+  #private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_vpc" "app" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
