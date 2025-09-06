abd3f1a11e65d93766863c32cc2a5b972fdf99a8
davejdeady <davidjohndeady@outlook.com>
2025-09-06 20:37:25 +0100

    s3 bucket and iam updates

--- m5_solution/s3.tf
+++ m5_solution/s3.tf
@@ -63,7 +63,7 @@ data "aws_iam_policy_document" "web_bucket" {
 
     resources = [
       aws_s3_bucket.web_bucket.arn,
-      "${aws_s3_bucket.web_bucket.arn}/alb-logs/*",]
+    "${aws_s3_bucket.web_bucket.arn}/alb-logs/*", ]
   }
 }
 
